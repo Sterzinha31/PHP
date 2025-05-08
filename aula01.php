8f997072047195b0b5325373b095d376fa500dca
Stephanie de Araújo <stephanie.araujo.farias@gmail.com>
2025-05-08 12:03:11 -0300

    Add files via upload

---
+++ aula01.php
@@ -0,0 +1,8 @@
+<h1>Minha primeira página PHP</h1>
+<?php
+echo "<p>Meu nome é Steh</p>"; // Exibe uma mensagem
+echo "<br>"; //Quebra linha
+echo"<p>Olá mundo</p>";
+echo "<p> Estou aprendendo PHP!</p>";
+echo "<p> Estou gostandi PHP!</p>";
+?>
