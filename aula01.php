967c0a5a82e45bbe4a336bdcf0185b8a0a807408
Stephanie de Araújo <stephanie.araujo.farias@gmail.com>
2025-05-09 10:49:46 -0300

    Add files via upload

--- aula01.php
+++ aula01.php
@@ -1,8 +1,24 @@
 <h1>Minha primeira página PHP</h1>
 <?php
-echo "<p>Meu nome é Steh</p>"; // Exibe uma mensagem
+echo "<p>Meu nome é Stephanie</p>"; // Exibe uma mensagem
 echo "<br>"; //Quebra linha
 echo"<p>Olá mundo</p>";
 echo "<p> Estou aprendendo PHP!</p>";
 echo "<p> Estou gostandi PHP!</p>";
+echo "<br>";
+
+// comentário de uma linha
+# comentário de uma linha
+//variáveis
+// usamoso simbolo $ para declarar uma variáve
+$nome = "Steh"; //string
+$idade = 17; // inteiro
+$peso =  48; // float
+$altura = 1.69; // 
+
+echo "Dados pessoais:<br>";
+echo "Meu apelido é $nome<br>";
+echo "Tenho $idade anos de idade";
+echo "Meu peso é $peso kg<br>";
+echo "Minha altura é $altura m<br>";
 ?>
